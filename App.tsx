c2334b3e85fc3d0de767eedfe65fb08c82aff519
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-03-14 17:25:34 -0400

    feat: added validation to prevent duplicated and to limit the number of images to be uploaded

--- App.tsx
+++ App.tsx
@@ -1,71 +1,101 @@
 import { StatusBar } from 'react-native';
-import { StyleSheet,Alert,  View, Button , Image, useWindowDimensions, FlatList } from 'react-native';
-import { useState, useEffect } from 'react';
+import { StyleSheet,Alert,  View,  FlatList , TouchableOpacity , Text, Image, ActivityIndicator} from 'react-native';
+import { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 
 
-type ImageListProps = Array<{    
-      id: string;
-      url: string;
-}>;
+type ImageListProps = Array<{
+  id: string;
+  source: string;
+}>
 
 
 export default function App() {
+  
   const [ images, setImages ] = useState<ImageListProps>([]);
-  const [ isLoading, setIsLoading ] = useState(true);
-  
-  const handleImagePicker = async()=>{
-    const limit: number = 10;
-    let remaining: number;
-    remaining = limit - images.length;
+  const [ isLoading, setIsLoading ] = useState(false); 
+  const [ limit, setLimit ] = useState(5)
 
-   
-    if(images.length === 10 ){
-      Alert.alert('No more images', 'You have reach the 5 images limit')
+  const getPickedImages = async()=>{
+    try{
+      // No permissions request is necessary for launching the image library
+    
+      let result = await ImagePicker.launchImageLibraryAsync({
+       mediaTypes: ImagePicker.MediaTypeOptions.Images,
+       // allowsEditing: true, **does not work for multiple images** 
+       allowsMultipleSelection: true,
+       selectionLimit: 5,
+       aspect: [4, 3 ],
+       quality: 1
+      })
+      
+      
+      if(!result.canceled){         
+        for(let asset of result.assets){ 
+          
+          console.log(result.assets.length)
+          console.log(images.length, 'linha39')
+          console.log( limit - (images.length + result.assets.length) , 'linha38')
+          if(result.assets.length + images.length > limit ){
+            return Alert.alert(`Total of ${limit} photos allowed`, 'Please review your selections.' )
+          }
+          
+          const duplicated = images.find(item => `${asset.fileName}${asset.assetId}` === item.id);
+        
+          if(duplicated){                  
+            return Alert.alert('Try again.', 'One of the images selected were duplicated');
+
+          }
+          
+          setImages(prevState => [ ...prevState, { id:`${asset.fileName}${asset.assetId}`, source:asset.uri }])
+          
+        }      
+      }
+     
+    }catch(error){
+     Alert.alert(`${error}`);
     }
-     // No permissions request is necessary for launching the image library
-     try{
-       let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        // allowsEditing: true, **does not work for multiple images** 
-        allowsMultipleSelection: true,
-        selectionLimit: 5,
-        aspect: [4, 3 ],
-        quality: 1
-       })
-      
-       if(!result.canceled){
-         for(let asset of result.assets){  
-          setImages(prevState => [ ...prevState, { id:`${asset.fileName}${asset.assetId}`, url:asset.uri }])
-      
-       }
-  
-       }
-      
-
-     }catch(error){
-      Alert.alert(`${error}`);
-     }
   }
+
+  const handleImagePicker = async()=>{    
+    setIsLoading(true);
+    await getPickedImages();
+    setIsLoading(false);
+  }
+
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
-       <View style={styles.buttonContainer}>
-        <Button
-          onPress={handleImagePicker}
-          title="Choose up to 10 images"/>
-      </View> 
-
+        { isLoading ? <ActivityIndicator size="large" color="#3bc20b"/> :      
+        (
+          <>
+        
         <FlatList 
-          showsVerticalScrollIndicator={false}
-          data={images}
-          keyExtractor={item => item.id}
-          renderItem={({item})=> <Image style={styles.image} src={item.url} />}
-          numColumns={2}
-          contentContainerStyle={{marginTop: 20, gap: 20}}
+      
+        showsVerticalScrollIndicator={false}
+        data={images}
+        keyExtractor={item => item.id}
+        renderItem={({item})=> <Image style={styles.image} source={{ uri: item.source}} />}
+        numColumns={2}
+        contentContainerStyle={{marginTop: 20, gap: 20, width: '100%'}}
+        ListHeaderComponent={
+          <View>
+            <TouchableOpacity 
+            disabled={images.length === limit}  
+            activeOpacity={0.7} 
+            onPress={handleImagePicker} 
+            style={[styles.buttonContainer, {backgroundColor: images.length === limit ? '#a9a9a9' : '#3bc20b'}]}>
+              <Text style={styles.buttonText}>{images.length === limit ? 'Unable to upload more images' : `Choose up to ${limit} images`}</Text>              
+            </TouchableOpacity> 
+          </View>
+
+        }
         />
+        </>)
+        
+      }
 
     </View>
   );
@@ -76,18 +106,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#202024',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
+  
   },
-  image:{
+   image:{
     width: 100,
     height: 100,
     borderRadius: 100, 
-    borderWidth:2, 
-    borderColor: '#F7A5A6', 
+    borderWidth:3, 
+    borderColor: '#3bc20b', 
     margin: 15 
   },
   buttonContainer:{
     marginTop: 100, 
-    marginBottom: 20
-  }
+    marginBottom: 20,
+    backgroundColor: '#3bc20b',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText:{
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
 });
